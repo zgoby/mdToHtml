febb3cb13d85630f6e515a345796ef39dc1ff468
郭志强 <zguoby@gmail.com>
2020-07-02 14:37:57 +0800

    can run through

--- mainGenerator.js
+++ mainGenerator.js
@@ -1,26 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 const ejs = require('ejs');
+const showdown = require('showdown'),converter = new showdown.Converter();
 
 class MainGenerator {
   firstLevelDir=[];
   dirs = {};
   mdContent = '';
-  constructor(floder) {
-    this.floder=floder
+  template;
+  bookName;
+  output;
+  constructor({folder, bookName, template, output}) {
+    this.folder=folder;
+    this.bookName=bookName;
+    this.template=template;
+    this.output=output;
   }
 
   directoryParse() {
     try{
-      const temFirst = fs.readdirSync(this.floder);
+      const temFirst = fs.readdirSync(this.folder);
       // firstLevelTitle = temFirst.sort((a, b)=>{
       //   if(parseInt(a)>=parseInt(b)) return 1;
       //   else return -1;
       // });
       this.firstLevelDir=this.pathFormat(temFirst);
       temFirst.forEach((item, index)=>{
-        const secondaryDir = fs.readdirSync(path.join(this.floder,item))
-        this.dirs[this.firstLevelDir[index]] = this.pathFormat(secondaryDir, true);
+        const secondaryDir = fs.readdirSync(path.join(__dirname, this.folder,item))
+        this.dirs[this.firstLevelDir[index]] = {
+          title: this.pathFormat(secondaryDir, true),
+          originalDir: item,
+          mdPath: secondaryDir.map(it => path.join(__dirname, this.folder, item, it)),
+        };
       });
     }catch(err) {
       throw err;
@@ -42,32 +53,34 @@ class MainGenerator {
     return { dirs: this.dirs, firstLevelDirs: this.firstLevelDir };
   }
 
-  toNavs(firstLevelDir, dirs) {
-    console.log(this.dirs)
+  generateHtmlWithMd(md) {
+    return converter.makeHtml(md);
   }
 
-  _generateHtmlWithMd(dirs) {
-    
+  generateTemplate() {
+    this.templateHtml = fs.readFileSync(path.join(__dirname, "template", this.template, 'index.html')).toString();
   }
 
-  _generateTemplate(template) {
-    this.templateHtml = fs.readFileSync(path.join("template", template, 'index.html')).toString();
-  }
-
-  toContents(template) {
-    this._generateTemplate(template);
-    const html = ejs.render(this.templateHtml, {
-      title: 'aaaaa',
-      dirs: this.dirs,
-      firstLevelDir: this.firstLevelDir,
-      active: this.firstLevelDir[0],
-      mdContent: this.mdContent,
+  build() {
+    this.generateTemplate();
+    this.firstLevelDir.forEach((item)=>{
+      const dirsItem = this.dirs[item];
+      dirsItem.mdPath.forEach((it, index)=>{
+        const mdBuf = fs.readFileSync(it);
+        const html = ejs.render(this.templateHtml, {
+          title: 'aaaaa',
+          dirs: this.dirs,
+          firstLevelDir: this.firstLevelDir,
+          active: item,
+          mdContent: this.generateHtmlWithMd(mdBuf.toString()),
+        });
+        // m目录待确定
+        // console.log(path.join('./builds/', item, dirsItem.title[index]+'.md'));
+        
+        const writeS = fs.createWriteStream(path.join(__dirname, this.output, item, dirsItem.title[index]+'.html'), 'utf8');
+        writeS.write(html)
+      })
     });
-    console.log(html);
-  }
-
-  toHome(params) {
-  
   }
 }
 
